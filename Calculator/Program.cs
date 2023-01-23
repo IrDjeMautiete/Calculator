c718c0e1a85176481e26a4fa75d74bd4bbb1afa8
IrDjeMautiete <irdjemautiete@gmail.com>
2023-01-23 14:29:29 +0100

    Ajoutez des fichiers projet.

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Linq;
+using Microsoft.VisualBasic.FileIO;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // FOUS TON CODE ICI !!!
+
+
+
+            Console.WriteLine("\nDurée d’exécution: " + stopwatch.Elapsed.TotalSeconds);
+        }
+    }
+}
